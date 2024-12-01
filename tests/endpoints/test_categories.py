942cb3912ca1445977369e67b4a714e8f90e65f3
Ilya Stetsun <ilya.st@halykmarket.com>
2024-12-01 20:59:27 +0500

    check amount of cpu github runner

--- tests/endpoints/test_categories.py
+++ tests/endpoints/test_categories.py
@@ -6,7 +6,7 @@ from models.game_models import GamesResponse
 @allure.feature("Categories API")
 @allure.story("Get games by category")
 @pytest.mark.parametrize("category_uuid, x_task_value", [("8126d35b-5336-41ad-981d-f245c3e05665", "api-10")])
-def test_search_games(api_client, category_uuid, x_task_value):
+def test_search_category_games(api_client, category_uuid, x_task_value):
     response = api_client.search_games_by_category(category_uuid, x_task_value)
     dto = GamesResponse(**response.json())
 
