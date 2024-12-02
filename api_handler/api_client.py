32b31b915a33063a8a600336edf78be9867933a8
Ilya Stetsun <ilya.st@halykmarket.com>
2024-12-02 17:59:57 +0500

    fix

--- api_handler/api_client.py
+++ api_handler/api_client.py
@@ -149,7 +149,7 @@ class ApiClient:
         #     "avatar_file": (file_name, file_content, "image/jpeg")
         # }
         files = {
-            "avatar_file": (file_name, open(file_path, 'rb'), "image/jpeg")  # Задаем имя файла, содержимое и MIME-тип
+            "avatar_file": (file_name, open(file_path, 'r'), "image/jpeg")  # Задаем имя файла, содержимое и MIME-тип
         }
         self.logger.info("created files object")
 
